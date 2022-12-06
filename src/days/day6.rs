52f4e6046d3ece60961d4d2c731fda2a0e87342e
Janosch Maier <janosch.maier@ottonova.de>
2022-12-06 08:20:06 +0100

    style: simplify day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub fn tasks(content: &String) -> (String, String) {
     let result1 = task1(content);
     let result2 = task2(content);
@@ -15,42 +17,19 @@ fn task2(content: &String) -> String {
 }
 
 fn check_unique(content: &String, unique_characters : usize) -> String {
-    let mut char_buff = String::from("");
+    let char_buff : Vec<char> = content.chars().collect_vec();
     let mut counter = 0;
-
-    for character in content.chars() {
-        counter += 1;
-
-        // First unique_characters need to be pushed in any case
-        if char_buff.len() < unique_characters {
-            char_buff.push(character);
-        // Afterwards we remove the first character and check for duplicates
-        } else {
-            char_buff.remove(0);
-            
-            if !char_buff.contains(character) && !has_duplicates(&char_buff) {
-                return counter.to_string();
-            }
-
-            char_buff.push(character);
+    
+    for c in char_buff.windows(unique_characters) {
+        if c.iter().all_unique() {
+            return (counter + unique_characters).to_string();
         }
+
+        counter += 1;
     }
 
     return String::from("");
 }
-
-fn has_duplicates(s: &str) -> bool {
-    let curr = s;
-    let mut checker = String::from(s);
-    for c in curr.chars() {
-        checker.remove(0).to_string();
-        if checker.contains(c) {
-            return true
-        }
-    }
-    return false;
-}
-
 
 #[test]
 fn test_task1() {
