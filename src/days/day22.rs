0de6cd1ba33d08ce5b6abe425a94f28cec62dbdc
Janosch Braukmann <janosch.braukmann@gmail.com>
2022-12-22 21:32:35 +0100

    feat: day 22 task 1

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,6 +1,8 @@
+use std::fmt;
+
 use grid::{Grid, grid};
 use parse_display::{Display, FromStr};
-use log::{debug, error};
+use log::{debug, trace};
 use rotate_enum::RotateEnum;
 
 pub fn tasks(content: &String) -> (String, String) {
@@ -11,15 +13,26 @@ pub fn tasks(content: &String) -> (String, String) {
 
 fn task1(content: &String) -> String {
     let (map, directions) = parse_input(content);
+
+    let mut c = 0;
+    for i in 0..map.cols() {
+        if map[0][i] == Tile::Open {
+            break;
+        }
+        c += 1;
+    }
+
     let mut pos = Pos {
-        row: 1,
-        column: 1,
+        row: 0,
+        column: c,
         facing: Facing::Right,
     };
 
     for d in directions {
         pos = pos.step(&map, d);
     }
+
+    draw(&map, pos);
 
     pos.get_result().to_string()
 }
@@ -28,7 +41,7 @@ fn task2(content: &String) -> String {
     String::from("")
 }
 
-#[derive(Debug, FromStr, Display)]
+#[derive(Debug, FromStr, Display, PartialEq, Eq)]
 enum Tile {
     #[display(" ")]
     None,
@@ -49,7 +62,7 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug, RotateEnum)]
+#[derive(Debug, RotateEnum, Clone, Copy)]
 enum Facing {
     Top,
     Right,
@@ -57,18 +70,52 @@ enum Facing {
     Left,
 }
 
-impl Facing {
-    fn to_i32(self) -> i32 {
-        match self {
-            Right => 0,
-            Down => 1,
-            Left => 2,
-            Top => 3
-        }
+impl fmt::Display for Facing {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let d = match self {
+            Facing::Top => "^",
+            Facing::Right => ">",
+            Facing::Bottom => "v",
+            Facing::Left => "<",
+        };
+        write!(f, "{}", d)
     }
 }
 
-#[derive(Debug)]
+impl Facing {
+    fn to_i32(self) -> i32 {
+        match self {
+            Facing::Right => 0,
+            Facing::Bottom => 1,
+            Facing::Left => 2,
+            Facing::Top => 3
+        }
+    }
+
+    fn get_direction(self) -> (i32, i32) {
+        let mut add_rows = 0;
+        let mut add_columns = 0;
+        match self {
+            Facing::Right => {
+                add_columns = 1;
+            },
+            Facing::Bottom => {
+                add_rows = 1;
+            },
+            Facing::Left => {
+                add_columns = -1;
+            },
+            Facing::Top => {
+                add_rows = -1;
+            },
+        }
+
+        (add_rows, add_columns)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Pos {
     row: i32,
     column: i32,
@@ -76,43 +123,46 @@ struct Pos {
 }
 
 impl Pos {
-    fn step(mut self, map: &Grid<Tile>, direction: Direction) -> Pos {
+    fn add(mut self, map: &Grid<Tile>, rows: i32, columns: i32) -> Pos {
+
+        self.row = (self.row + rows).rem_euclid(map.rows() as i32);
+        self.column = (self.column + columns).rem_euclid(map.cols() as i32);
+
+        self
+    }
+
+    fn step(self, map: &Grid<Tile>, direction: Direction) -> Pos {
+        let mut pos = self.clone();
 
         match direction {
             Direction::Steps(s) => {
-                for i in 0..s {
-                    let mut column = self.column;
-                    let mut row = self.row;
-                    match self.facing {
-                        Facing::Right => {
-                            
-                        
-                        },
-                        Facing::Bottom => {
-
-                        },
-                        Facing::Left => {
-                            
-                        },
-                        Facing::Top => {
-
-                        },
+                let (add_rows, add_columns) = &self.facing.get_direction();
+                for i in 0..s {  
+                    let mut new_pos = pos.add(map, *add_rows, *add_columns);
+                    while map[new_pos.row as usize][new_pos.column as usize] == Tile::None {
+                        new_pos = new_pos.add(map, *add_rows, *add_columns);
+                        trace!("New Position: {:?}", new_pos);
+                    }
+                    if map[new_pos.row as usize][new_pos.column as usize] == Tile::Open {
+                        pos = new_pos
+                    }
+                    if map[new_pos.row as usize][new_pos.column as usize] == Tile::Wall {
+                        break;
                     }
                 }
-                println!("Moving {} steps forward. New position is: {:?}", s, self);
+                debug!("Moving {} steps forward. New position is: {:?}", s, pos);
             }
             Direction::Left => {
-                self.facing = self.facing.prev();
-                println!("Turning Left. New position is: {:?}", self);
+                pos.facing = self.facing.prev();
+                debug!("Turning Left. New position is: {:?}", pos);
             }
             Direction::Right => {
-                self.facing = self.facing.next();
-                println!("Turning Right. New position is: {:?}", self);
+                pos.facing = self.facing.next();
+                debug!("Turning Right. New position is: {:?}", pos);
             }
         }
-        println!();
 
-        self
+        pos
     }
 
     fn get_result(self) -> i32 {
@@ -158,14 +208,34 @@ fn parse_input(content: &String) -> (Grid<Tile>, Vec<Direction>) {
             directions.push(direction);
             steps = "".to_string();
         } else {
-            steps.push(c);
+            if c != '\n' {
+                steps.push(c);
+            }
         }
+    }
+    if steps.len() > 0 {
+        let direction: Direction = steps.parse().unwrap();
+        directions.push(direction);
     }
 
     debug!("{:?}", map);
     debug!("{:?}", directions);
 
     (map, directions)
+}
+
+fn draw(map: &Grid<Tile>, pos: Pos) {
+    for row in 0..map.rows() {
+        for column in 0..map.cols() {
+            if (pos.row as usize) == row && (pos.column as usize) == column {
+                print!("{}", pos.facing);
+            } else {
+                print!("{}", map[row][column]);
+            }
+        }
+        println!()
+    }
+    println!()
 }
 
 #[cfg(test)]
@@ -202,5 +272,4 @@ fn test_task1() {
 #[test]
 fn test_task2() {
     assert_eq!(task2(&test_input()), "");
-    assert_eq!(task2(&test_input2()), "");
 }
