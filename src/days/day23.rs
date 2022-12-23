3587b150b53c723a71a43796e62ef856b398c9ec
Janosch Braukmann <janosch.braukmann@gmail.com>
2022-12-24 00:25:29 +0100

    feat: day 23 task 2 but runs very long

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use log::{debug, error};
 
 pub fn tasks(content: &String) -> (String, String) {
@@ -18,45 +20,9 @@ fn task1(content: &String) -> String {
     for _ in 0..10 {
         //draw(&positions);
 
-        let mut potential_positions: Vec<Pos> = vec![];
-
         // Setup New Positions
-        for pos in &positions {
-            if !pos.any_neighbours(&positions) {
-                // No Neighbors, So we are not moving.
-                debug!("Elf at pos {:?} has no neighbors and does not move", pos);
-                potential_positions.push(*pos);
-            } else {
-                // We have a neighbour, so we try to move
-                let mut new_pos = pos.clone();
-                for direction in &directions {
-                    new_pos = pos.walk(direction, &positions);
-                    if new_pos != *pos {
-                        debug!("Elf at pos {:?} moves {:?} to {:?}", pos, direction, new_pos);
-                        break;
-                    }
-                }
-                potential_positions.push(new_pos);
-            }
-        }
-
-        // Actually Move
-        let mut new_positions: Vec<Pos> = vec![];
-        for i in 0..positions.len() {
-
-            let old_pos = positions[i];
-            let new_pos = potential_positions[i];
-
-            let mut position_checker = potential_positions.clone();
-            position_checker.remove(i);
-
-            if !position_checker.contains(&new_pos) {
-                new_positions.push(new_pos);
-            } else {
-                new_positions.push(old_pos);
-            }
-        }
-        positions = new_positions;
+        let potential_positions = get_potential_positions(&positions, &directions);
+        positions = move_to_new_positions(&positions, &potential_positions);
 
         let changed_direction = directions.remove(0);
         directions.push(changed_direction);
@@ -65,12 +31,37 @@ fn task1(content: &String) -> String {
     draw(&positions);
 
     let (minx, miny, maxx, maxy) = square_edges(&positions);
-    ((maxx + 1- minx) * (maxy + 1 - miny) - positions.len() as i32).to_string()
-
+    ((maxx + 1 - minx) * (maxy + 1 - miny) - positions.len() as i32).to_string()
 }
 
 fn task2(content: &String) -> String {
-    String::from("")
+    
+    let mut positions = vec![];
+    let mut new_positions = parse_input(content);
+    let mut directions: Vec<Direction> = vec![
+        Direction::North,
+        Direction::South,
+        Direction::West,
+        Direction::East,
+    ];
+    let mut rounds = -1;
+
+    draw(&new_positions);
+
+    while positions != new_positions {
+        rounds += 1;
+        
+        positions = new_positions.clone();
+        
+        let potential_positions = get_potential_positions(&new_positions, &directions);
+        new_positions = move_to_new_positions(&new_positions, &potential_positions);
+        draw(&new_positions);
+        
+        let changed_direction = directions.remove(0);
+        directions.push(changed_direction);
+    }
+
+    rounds.to_string()
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
@@ -178,6 +169,53 @@ fn square_edges(positions: &Vec<Pos>) -> (i32, i32, i32, i32) {
     (minx, miny, maxx, maxy)
 }
 
+fn get_potential_positions(positions: &Vec<Pos>, directions: &Vec<Direction>) -> Vec<Pos> {
+    let mut potential_positions: Vec<Pos> = vec![];
+
+    for pos in positions {
+        if !pos.any_neighbours(&positions) {
+            // No Neighbors, So we are not moving.
+            println!("Elf at pos {:?} has no neighbors and does not move", pos);
+            potential_positions.push(*pos);
+        } else {
+            // We have a neighbour, so we try to move
+            let mut new_pos = pos.clone();
+            for direction in directions {
+                new_pos = pos.walk(direction, &positions);
+                if new_pos != *pos {
+                    debug!(
+                        "Elf at pos {:?} moves {:?} to {:?}",
+                        pos, direction, new_pos
+                    );
+                    break;
+                }
+            }
+            potential_positions.push(new_pos);
+        }
+    }
+
+    potential_positions
+}
+
+fn move_to_new_positions(mut positions: &Vec<Pos>, potential_positions: &Vec<Pos>) -> Vec<Pos> {
+    let mut new_positions: Vec<Pos> = vec![];
+    for i in 0..positions.len() {
+        let old_pos = positions[i];
+        let new_pos = potential_positions[i];
+
+        let mut position_checker = potential_positions.clone();
+        position_checker.remove(i);
+
+        if !position_checker.contains(&new_pos) {
+            new_positions.push(new_pos);
+        } else {
+            new_positions.push(old_pos);
+        }
+    }
+
+    new_positions
+}
+
 fn parse_input(content: &String) -> Vec<Pos> {
     let mut positions: Vec<Pos> = vec![];
 
@@ -230,6 +268,6 @@ fn test_task1() {
 
 #[test]
 fn test_task2() {
-    assert_eq!(task2(&test_input()), "");
-    assert_eq!(task2(&test_input2()), "");
+    assert_eq!(task2(&test_input2()), "3");
+    assert_eq!(task2(&test_input()), "20");
 }
