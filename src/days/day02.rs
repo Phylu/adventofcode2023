a8da0ff0415a7fa364801e902c9d11c2f167e926
Janosch Maier <janosch.maier@ottonova.de>
2023-12-05 20:32:28 +0100

    feat: solve day 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -7,46 +7,103 @@ pub fn tasks(content: &String) -> (String, String) {
 }
 
 fn task1(content: &String) -> String {
-    let vec = prepare_input(content);
+    let vec = prepare_input_1(content);
     let result: i32 = vec.iter().sum();
 
     return result.to_string();
 }
 
 fn task2(content: &String) -> String {
-//    let vec = prepare_input_2(content);
-//    let result: i32 = vec.iter().sum();
+    let vec = prepare_input_2(content);
+    let result: i32 = vec.iter().sum();
 
-//    return result.to_string();
-    return "".to_string();
+   return result.to_string();
 }
 
-fn prepare_input(content: &String) -> Vec<i32> {
+fn prepare_input_1(content: &String) -> Vec<i32> {
 
     let mut vec = Vec::new();
 
     let max_red = 12;
-    let max_greens = 13;
+    let max_green = 13;
     let max_blue = 14;
 
-    for line in content.lines() {
+    'lines: for line in content.lines() {
 
         if line != "" {
             let game_re = Regex::new(r"Game (?P<game>\d+):").unwrap();
             let game_captures = game_re.captures(line).unwrap();
             let game_number = String::from(&game_captures["game"]);
 
-            let blues_re = Regex::new(r"(?P<blue>\d+) blue").unwrap();
-            let blue_captures = blues_re.captures_iter(line);
-            blue_captures.map(|capture| {
-                let blues: i32 = capture.name("blue").unwrap().as_str().parse().unwrap();
-                if blues > max_blue {
-                    continue;
+            let reds_re = Regex::new(r"(?P<red>\d+) red").unwrap();
+            for reds in reds_re.captures_iter(line) {
+                let int_reds: i32 = reds["red"].parse().unwrap();
+                if int_reds > max_red {
+                    continue 'lines;
                 }
-            } );
+            }
 
-            println!("{}", game_number);
+            let greens_re = Regex::new(r"(?P<green>\d+) green").unwrap();
+            for greens in greens_re.captures_iter(line) {
+                let int_greens: i32 = greens["green"].parse().unwrap();
+                if int_greens > max_green {
+                    continue 'lines;
+                }
+            }
+
+            let blues_re = Regex::new(r"(?P<blue>\d+) blue").unwrap();
+            for blues in blues_re.captures_iter(line) {
+                let int_blues: i32 = blues["blue"].parse().unwrap();
+                if int_blues > max_blue {
+                    continue 'lines;
+                }
+            }
+
             vec.push(format!("{}", game_number).parse().unwrap());
+            
+        }
+    }
+    
+    return vec;
+}
+
+fn prepare_input_2(content: &String) -> Vec<i32> {
+
+    let mut vec = Vec::new();
+
+    for line in content.lines() {
+
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        if line != "" {
+            
+            let reds_re = Regex::new(r"(?P<red>\d+) red").unwrap();
+            for reds in reds_re.captures_iter(line) {
+                let int_reds: i32 = reds["red"].parse().unwrap();
+                if int_reds > max_red {
+                    max_red = int_reds;
+                }
+            }
+
+            let greens_re = Regex::new(r"(?P<green>\d+) green").unwrap();
+            for greens in greens_re.captures_iter(line) {
+                let int_greens: i32 = greens["green"].parse().unwrap();
+                if int_greens > max_green {
+                    max_green = int_greens;
+                }
+            }
+
+            let blues_re = Regex::new(r"(?P<blue>\d+) blue").unwrap();
+            for blues in blues_re.captures_iter(line) {
+                let int_blues: i32 = blues["blue"].parse().unwrap();
+                if int_blues > max_blue {
+                    max_blue = int_blues
+                }
+            }
+
+            vec.push(format!("{}", max_red * max_green * max_blue).parse().unwrap());
             
         }
     }
@@ -60,10 +117,9 @@ fn test_task1() {
     assert_eq!(task1(&content), "8");
 }
 
-/*
+
 #[test]
 fn test_task2() {
     let content = std::fs::read_to_string("input_test/2.txt").unwrap(); 
-    assert_eq!(task2(&content), "281");
+    assert_eq!(task2(&content), "2286");
 }
-*/
